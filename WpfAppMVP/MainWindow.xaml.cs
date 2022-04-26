a9ee2cbeb9e102a4ec9a8017e10f76a03cf00dc0
StasErshov <StasErshov@WIN-NRC48IA0QHK>
2022-04-26 18:18:23 +0300

    Добавьте файлы проекта.

---
+++ WpfAppMVP/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+using ProjectBilling.Business;
+
+namespace ProjectBilling.UI.MVP
+{
+    public partial class MainWindow : Window
+    {
+        private IProjectsModel model = null;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            model = new ProjectsModel();
+        }
+
+        private void showProjectsButton_Click(object sender,
+            RoutedEventArgs e)
+        {
+            ProjectsView view = new ProjectsView();
+            ProjectsPresenter presenter
+                = new ProjectsPresenter(view, model);
+            view.Owner = this;
+            view.Show();
+        }
+    }
+}
